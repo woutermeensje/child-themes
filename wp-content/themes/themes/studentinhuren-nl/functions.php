fc24b64a479edc29b6e39682c8a75259ac536a96
WouterMeens <w.m.meens@gmail.com>
2026-07-17 19:05:20 +0200

    upda

--- wp-content/themes/themes/studentinhuren-nl/functions.php
+++ wp-content/themes/themes/studentinhuren-nl/functions.php
@@ -252,7 +252,6 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('inter-font', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap', [], null);
     wp_enqueue_style('work-sans-font', 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@600;700;800;900&display=swap', [], null);
     wp_enqueue_style('roboto-font', 'https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap', [], null);
-    wp_enqueue_style('custom-fonts', get_stylesheet_directory_uri() . '/fonts/fonts.css');
 
     // Globale knoppen
     if (file_exists(get_stylesheet_directory() . '/css/buttons.css')) {
